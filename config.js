b99d1efe2aebccdb171208d93b04f68c4f4bb839
Josh XT <josh@devxt.com>
2023-08-23 12:44:39 -0400

    Updates

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:7437",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
+};
+
+export default config;
--- next.config.js
+++ next.config.js
@@ -1,39 +1,16 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
-
 module.exports = {
-  webpack(config) {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    };
+
     config.module.rules.push({
       test: /\.tsx?$/,
       use: "ts-loader",
       exclude: /node_modules/,
     });
-
-    return config;
-  },
-
-  // Configure Babel
-  babel: {
-    presets: ["next/babel"],
-    plugins: [
-      [
-        "@babel/plugin-transform-typescript",
-        {
-          isTSX: true,
-        },
-      ],
-    ],
-  },
-
-  // Alias agixt to dist
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      agixt$: path.resolve(__dirname, "node_modules/agixt/dist/index.js"),
-    };
 
     return config;
   },
