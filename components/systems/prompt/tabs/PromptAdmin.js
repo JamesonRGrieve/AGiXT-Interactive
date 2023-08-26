b0ac7f67ea6b5b54cbaedda306b4f7e4b3ff57e8
Josh XT <josh@devxt.com>
2023-08-26 15:48:40 -0400

    Added prompt category selector

--- components/systems/prompt/tabs/PromptAdmin.js
+++ components/systems/prompt/tabs/PromptAdmin.js
@@ -3,7 +3,14 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { mutate } from "swr";
 import { sdk } from "../../../../lib/apiClient";
-import { TextField, Button, Divider, Container } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Divider,
+  Container,
+  Select,
+  MenuItem,
+} from "@mui/material";
 // TODO: Add prompt category field and logic to choose category before choosing prompt, setting to "Default" for now.
 const promptCategory = "Default";
 export default function PromptAdmin({ friendly_name, name, args, enabled }) {
@@ -13,8 +20,13 @@ export default function PromptAdmin({ friendly_name, name, args, enabled }) {
     "prompt/" + promptName,
     async () => await sdk.getPrompt(promptName, promptCategory)
   );
+  const promptCategories = useSWR(
+    "promptCategory",
+    async () => await sdk.getPromptCategories()
+  );
   const [newName, setNewName] = useState(promptName);
   const [newBody, setNewBody] = useState(prompt.data);
+  const [promptCategory, setPromptCategory] = useState("Default");
   console.log(prompt);
   const handleDelete = async () => {
     await sdk.deletePrompt(promptName, promptCategory);
@@ -26,8 +38,24 @@ export default function PromptAdmin({ friendly_name, name, args, enabled }) {
     mutate(`prompt`);
     router.push(`/prompt/${newName}`);
   };
+
   return (
     <Container>
+      <Select
+        fullWidth
+        variant="outlined"
+        label="Prompt Category"
+        value={promptCategory}
+        onChange={(e) => {
+          setPromptCategory(e.target.value);
+        }}
+      >
+        {promptCategories.data.map((category) => (
+          <MenuItem key={category} value={category}>
+            {category}
+          </MenuItem>
+        ))}
+      </Select>
       <TextField
         fullWidth
         variant="outlined"
