9c8715abb5e5ac6959da1984ef82e86532ab423a
James Grieve <jamesonrgrieve@gmail.com>
2023-09-05 13:17:29 -0600

    Improved interactions footer;

--- components/menu/PopoutDrawerWrapper.js
+++ components/menu/PopoutDrawerWrapper.js
@@ -12,7 +12,6 @@ const rightDrawerWidth = 320;
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'openLeft' && prop !== 'openRight' })(
     ({ theme, openLeft, openRight }) => ({
         flexGrow: 1,
-        padding: theme.spacing(3),
         transition: theme.transitions.create('margin', {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
