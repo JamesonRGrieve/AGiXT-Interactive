5f5898b139dcb96b05bc88a1ae87221c695b6310
Josh XT <josh@devxt.com>
2023-09-07 13:07:17 -0400

    Inject advanced option vars in training

--- pages/train/index.js
+++ pages/train/index.js
@@ -1,6 +1,16 @@
 import { useRouter } from "next/router";
 import TrainPanel from "../../components/systems/train/TrainPanel";
-export default function Train() {
+export default function Train({
+  collectionNumber = 0,
+  limit = 10,
+  minRelevanceScore = 0.0,
+}) {
   const router = useRouter();
-  return <TrainPanel />;
+  return (
+    <TrainPanel
+      collectionNumber={collectionNumber}
+      limit={limit}
+      minRelevanceScore={minRelevanceScore}
+    />
+  );
 }
