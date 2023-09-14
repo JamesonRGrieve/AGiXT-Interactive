ccf58382b13b9e2b893922cd2dcecba35a5fad8a
Josh XT <josh@devxt.com>
2023-09-14 10:11:52 -0400

    Add arXiv reader

---
+++ components/systems/train/tabs/ArxivTraining.js
@@ -0,0 +1,81 @@
+import {
+  Button,
+  TextField,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { sdk } from "../../../../lib/apiClient";
+import { Check } from "@mui/icons-material";
+
+export default function ArxivTraining({ collectionNumber = 0 }) {
+  const [text, setText] = useState("");
+  const router = useRouter();
+  const [learnStatus, setLearnStatus] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [useArticleNumbers, setUseArticleNumbers] = useState(false);
+  const [maxResults, setMaxResults] = useState(5);
+  const agentName = router.query.agent;
+
+  const onTrain = async (text) => {
+    setLearnStatus("Please wait...");
+    await sdk.learnArxiv(agentName, userInput, text, collectionNumber);
+    setLearnStatus(`${agentName} has finished learning from the text.`);
+    setText("");
+  };
+
+  return (
+    <>
+      <Typography>
+        You can either enter a search query or enter specific article numbers to
+        read into the agents long term memory.
+      </Typography>
+      <br />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={useArticleNumbers}
+            onChange={(e) => setUseArticleNumbers(e.target.checked)}
+          />
+        }
+        label="Use specific article numbers instead of search query"
+      />
+      <br />
+      {!useArticleNumbers ? ( // Max results number field if it is a query
+        <>
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="Enter a search query to use on arXiv.org"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="Enter the maximum number of results to return"
+            value={maxResults}
+            onChange={(e) => setMaxResults(e.target.value)}
+          />
+        </>
+      ) : (
+        <>
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="Enter a comma separated list of article numbers to use on arXiv.org"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+        </>
+      )}
+      <br />
+      <Button variant="contained" color="primary" onClick={() => onTrain(text)}>
+        Train from Text
+      </Button>
+      <Typography>{learnStatus}</Typography>
+    </>
+  );
+}
