663f4c0f2aae525445eaa6eb1f24f4721f559b6d
Josh XT <josh@devxt.com>
2023-09-19 00:22:07 -0400

    Improve UI, still needs work

--- components/systems/chain/tabs/partial/ChainStep.js
+++ components/systems/chain/tabs/partial/ChainStep.js
@@ -14,6 +14,9 @@ import {
   AccordionDetails,
   AccordionSummary,
   Avatar,
+  Container,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
 import {
   ArrowCircleUp,
@@ -39,7 +42,6 @@ export default function ChainStep({
   const [promptName, setPromptName] = useState(pn);
   const [promptArgs, setPromptArgs] = useState(prompt);
   const [promptCategory, setPromptCategory] = useState("Default");
-  const [expanded, setExpanded] = useState(false);
   const [stepType, setStepType] = useState(-1);
   const router = useRouter();
   const [modified, setModified] = useState(false);
@@ -87,9 +89,7 @@ export default function ChainStep({
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [prompt_type]);
-  const handleChange = () => {
-    setExpanded((old) => !old);
-  };
+
   const handleIncrement = async () => {
     await sdk.moveStep(router.query.chain, step, Number(step) + 1);
     mutate("chain/" + router.query.chain);
@@ -144,162 +144,53 @@ export default function ChainStep({
   };
   console.log("Prompt Type: ", prompt_type);
   return (
-    <>
-      <Paper
-        elevation={5}
-        sx={{
-          padding: "0.5rem",
-          display: "flex",
-          flexDirection: "column",
-          my: "1rem",
-          fontSize: "1rem",
+    <Container>
+      <IconButton onClick={handleDecrement} size="large" disabled={step == 1}>
+        <ArrowCircleUp sx={{ fontSize: "2rem" }} />
+      </IconButton>
+      <Typography>Step {step}</Typography>
+      <IconButton onClick={handleIncrement} size="large" disabled={last_step}>
+        <ArrowCircleDown sx={{ fontSize: "2rem" }} />
+      </IconButton>
+      <FormControl sx={{ mb: 2, width: "30%" }}>
+        <InputLabel>Type</InputLabel>
+        <Select
+          label="Type"
+          sx={{ mx: "0.5rem" }}
+          value={stepType}
+          onChange={(e) => {
+            setStepType(e.target.value);
+            setModified(true);
+          }}
+        >
+          <MenuItem value={-1}>Select a Type...</MenuItem>
+          {step_types.map((type, index) => {
+            return (
+              <MenuItem key={index} value={index}>
+                {type.name.replace(/\b\w/g, (s) => s.toUpperCase())}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+      <TextField
+        label="Agent Name"
+        sx={{ mx: "0.5rem" }}
+        value={agentName}
+        onChange={(e) => {
+          setAgentName(e.target.value);
+          setModified(true);
         }}
-      >
-        <Accordion expanded={expanded} onChange={handleChange}>
-          <AccordionSummary
-            sx={{ flexDirection: "row-reverse", alignItems: "center" }}
-            expandIcon={<ExpandCircleDownOutlined />}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                mx: "0.5rem",
-                flex: 1,
-              }}
-            >
-              {expanded ? null : (
-                <Typography variant="h6" sx={{ mr: "2rem" }}>
-                  Step Inputs
-                </Typography>
-              )}
-              <Box
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                  flex: 1,
-                }}
-              >
-                <IconButton
-                  onClick={handleDecrement}
-                  size="large"
-                  disabled={step == 1}
-                >
-                  <ArrowCircleUp sx={{ fontSize: "2rem" }} />
-                </IconButton>
-                <Avatar sx={{ fontWeight: "bolder" }}>{step}</Avatar>
-                <IconButton
-                  onClick={handleIncrement}
-                  size="large"
-                  disabled={last_step}
-                >
-                  <ArrowCircleDown sx={{ fontSize: "2rem" }} />
-                </IconButton>
-                <Select
-                  label="Type"
-                  sx={{ mx: "0.5rem" }}
-                  value={stepType}
-                  onChange={(e) => {
-                    setStepType(e.target.value);
-                    setModified(true);
-                  }}
-                >
-                  <MenuItem value={-1}>Select a Type...</MenuItem>
-                  {step_types.map((type, index) => {
-                    return (
-                      <MenuItem key={index} value={index}>
-                        {type.name.replace(/\b\w/g, (s) => s.toUpperCase())}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-                {stepType !== -1 ? step_types[stepType].component : null}
-                {modified ? (
-                  <IconButton onClick={handleSave} size="large">
-                    <SaveRounded sx={{ fontSize: "2rem" }} />
-                  </IconButton>
-                ) : null}
-                <IconButton onClick={handleDelete} size="large">
-                  <HighlightOff sx={{ fontSize: "2rem" }} />
-                </IconButton>
-              </Box>
-            </Box>
-          </AccordionSummary>
-          <AccordionDetails>
-            {expanded ? (
-              <Typography variant="h6">Step Inputs</Typography>
-            ) : null}
-            <Paper
-              elevation={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                my: "0.5rem",
-                mx: "2rem",
-                p: "0.3rem",
-              }}
-            >
-              <IconButton size="large">
-                <HighlightOff sx={{ fontSize: "2rem" }} />
-              </IconButton>
-              <Select label="Type" sx={{ mx: "0.5rem" }} value={0}>
-                <MenuItem value={0}>Select an Input Type...</MenuItem>
-              </Select>
-            </Paper>
-            <Paper
-              elevation={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                my: "0.5rem",
-                mx: "2rem",
-                p: "0.3rem",
-              }}
-            >
-              <IconButton size="large">
-                <HighlightOff sx={{ fontSize: "2rem" }} />
-              </IconButton>
-              <Select label="Type" sx={{ mx: "0.5rem" }} value={0}>
-                <MenuItem value={0}>Saved Output</MenuItem>
-              </Select>
-              <Select label="Output" sx={{ mx: "0.5rem" }} value={0}>
-                <MenuItem value={0}>&quot;Step 3 Output&quot;</MenuItem>
-              </Select>
-              <TextField variant="outlined" value="{step3}"></TextField>
-            </Paper>
-            <Paper
-              elevation={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                my: "0.5rem",
-                mx: "2rem",
-                p: "0.3rem",
-              }}
-            >
-              <IconButton size="large">
-                <HighlightOff sx={{ fontSize: "2rem" }} />
-              </IconButton>
-              <Select label="Type" sx={{ mx: "0.5rem" }} value={0}>
-                <MenuItem value={0}>Literal Value</MenuItem>
-              </Select>
-              <TextField
-                variant="outlined"
-                value="console.log('Hello, World!');"
-              ></TextField>
-              <TextField variant="outlined" value="{myLiteral}"></TextField>
-            </Paper>
-          </AccordionDetails>
-        </Accordion>
-      </Paper>
-    </>
+      />
+      {stepType !== -1 ? step_types[stepType].component : null}
+      {modified ? (
+        <IconButton onClick={handleSave} size="large">
+          <SaveRounded sx={{ fontSize: "2rem" }} />
+        </IconButton>
+      ) : null}
+      <IconButton onClick={handleDelete} size="large">
+        <HighlightOff sx={{ fontSize: "2rem" }} />
+      </IconButton>
+    </Container>
   );
 }
