2b6ac19b5d9446720a7faabbbc805fa30db9a347
Josh XT <josh@devxt.com>
2023-09-21 08:17:09 -0400

    Add chain import

--- pages/new/chain.js
+++ pages/new/chain.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { Container, TextField, Button, Typography } from "@mui/material";
+import { Container, TextField, Button, Typography, Input } from "@mui/material";
 import { mutate } from "swr";
 import { sdk } from "../../lib/apiClient";
 export default function Home() {
@@ -11,31 +11,48 @@ export default function Home() {
     mutate("chain");
     router.push(`/chain/${name}?agent=${router.query.agent}`);
   };
+  const handleFileUpload = async (event) => {
+    const files = Array.from(event.target.files);
+    for (let file of files) {
+      const fileContent = await file.text();
+      if (name == "") {
+        const fileName = file.name.replace(".json", "");
+        setName(fileName);
+      }
+      const steps = JSON.parse(fileContent);
+      await sdk.addChain(name);
+      await sdk.importChain(name, steps);
+      mutate("chain");
+      router.push(`/chain/${name}?agent=${router.query.agent}`);
+    }
+  };
 
   return (
     <Container>
       <Typography variant="h6" component="h2" marginY={"1rem"}>
         Add a New Chain
       </Typography>
-      <form>
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Chain Name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleCreate}
-          sx={{ marginY: "1rem" }}
-        >
-          Add a New Chain
-        </Button>
-      </form>
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Chain Name"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleCreate}
+        sx={{ marginY: "1rem" }}
+      >
+        Add a New Chain
+      </Button>
+      <Typography variant="h6" component="h2" marginY={"1rem"}>
+        Import a Chain
+      </Typography>
+      <Input type="file" onChange={handleFileUpload} />
     </Container>
   );
 }
