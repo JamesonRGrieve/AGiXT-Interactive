c0daf7b05214931609f851aa8ddb17b4503a2eb4
Josh XT <josh@devxt.com>
2023-10-15 12:35:08 -0400

    Reduce next code

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
 import "../styles/globals.css";
-import PropTypes from "prop-types";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
-
-App.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
