b72e32f6a22f650b1e345e5610e38bff44fadbea
Josh XT <josh@devxt.com>
2023-10-18 23:20:34 -0400

    Updates

--- pages/VeryHelpfulAI.js
+++ pages/VeryHelpfulAI.js
@@ -2,7 +2,7 @@ import AGiXTChat from "../components/AGiXTChat";
 export default function Postgres({}) {
   return (
     <AGiXTChat
-      baseUri={"http://localhost:7437"}
+      baseUri={"http://192.168.1.53:7437"}
       apiKey={""}
       agentName={"VeryHelpful.AI"}
       mode={"chain"}
