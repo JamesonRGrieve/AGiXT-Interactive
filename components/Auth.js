9e4eedccf0f08be1f45ceee2f178b48bc5887216
Josh XT <josh@devxt.com>
2023-10-21 15:04:27 -0400

    Clean up auth component

--- components/Auth.js
+++ components/Auth.js
@@ -1,72 +1,34 @@
-import {
-  Button,
-  TextField,
-  Container,
-  Typography,
-  Tooltip,
-} from "@mui/material";
+import { Button, TextField, Container, Typography } from "@mui/material";
 import { setCookie } from "cookies-next";
 
 export default function Auth({ username, userKey, setLoggedIn }) {
   const handleLogin = async () => {
-    if (process.env.USING_JWT !== "true") {
-      const authData = btoa(`${username}:${userKey}`);
-      if (authData) {
-        setCookie("apiKey", authData);
-        setLoggedIn(true);
-        setCookie("loggedIn", true);
-      }
-      return;
-    } else {
-      setCookie("apiKey", userKey);
+    const authData = btoa(`${username}:${userKey}`);
+    if (authData) {
+      setCookie("apiKey", authData);
       setLoggedIn(true);
       setCookie("loggedIn", true);
     }
   };
-  // If USING_JWT env var is true, then we need a username and password screen to go to auth provider
-  if (process.env.USING_JWT !== "true") {
-    return (
-      <Container>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Login
-        </Typography>
-        <TextField
-          label="Enter your username"
-          type="text"
-          value={username}
-          fullWidth
-        />
-        <br />
-        <TextField
-          label="Enter your password"
-          type="text"
-          value={userKey}
-          fullWidth
-        />
-        <br />
-        <Button
-          color="info"
-          variant="outlined"
-          onClick={handleLogin}
-          sx={{ height: "54px" }}
-          fullWidth
-        >
-          Log in
-        </Button>
-      </Container>
-    );
-  }
   return (
     <Container>
       <Typography variant="h5" component="h2" gutterBottom>
         Login
       </Typography>
       <TextField
+        label="Enter your username"
+        type="text"
+        value={username}
+        fullWidth
+      />
+      <br />
+      <TextField
         label="Enter your password"
         type="text"
         value={userKey}
         fullWidth
       />
+      <br />
       <Button
         color="info"
         variant="outlined"
