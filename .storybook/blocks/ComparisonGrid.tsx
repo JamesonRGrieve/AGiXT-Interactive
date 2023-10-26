6b6f76eb85d82c3ee38d2d78f23d35797db37f35
James Grieve <jamesonrgrieve@gmail.com>
2023-10-26 12:45:59 -0600

    Cleaned up blocks;

--- .storybook/blocks/ComparisonGrid.tsx
+++ .storybook/blocks/ComparisonGrid.tsx
@@ -1,43 +1,40 @@
-import { Primary, Story, useOf } from '@storybook/blocks';
-
-import { Box, Typography } from '@mui/material';
+import { Story, useOf } from '@storybook/blocks';
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
-import { withTheme } from '../preview';
 interface ComparisonGridProps {
   of?: any
 }
 export default function ComparisonGrid({ of }: ComparisonGridProps) {
   const resolvedOf = useOf('story', ['story', 'meta']);
-  
+
   switch (resolvedOf.type) {
     case 'story': {
-      return resolvedOf.story.parameters?.references.map((storySet: any) => {
-      if (!(storySet.images.filter((image: any) => image.primary).length > 0)) return null;
-      const primaryReference = storySet.images.filter((image: any) => image.primary)[0];
-      const primaryImage = primaryReference.image as StaticImageData;
-      const component = typeof resolvedOf.story.component == 'function' ? withTheme(resolvedOf.story.component({ ...resolvedOf.story.initialArgs }), { globals: { theme: 'light' } }) : <Box></Box>;
-      console.log(primaryReference);
       return <>
-        <h3>Reference Comparison</h3>
-        <div style={{
-          display: 'grid', gap: '1rem', gridTemplateColumns: '1fr 1fr', gridTemplateRows: '1fr', marginBottom: '3rem'
-        }}>
-          <div>
-            <h4>Reference Image</h4>
-            <div style={{ width: '100%', height: '100%', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }}>
-              <Image style={{ width: '100%', objectFit: 'contain' }} {...primaryImage} alt='Mockup Image' />
+        <h3>Reference Image Comparisons With Component</h3>
+        {resolvedOf.story.parameters?.references.map((storySet: any) => {
+          if (!(storySet.images.filter((image: any) => image.primary).length > 0)) return null;
+          const primaryReference = storySet.images.filter((image: any) => image.primary)[0];
+          const primaryImage = primaryReference.image as StaticImageData;
+          return <>
+            <div style={{
+              display: 'grid', gap: '1rem', gridTemplateColumns: '1fr 1fr', gridTemplateRows: '1fr', marginBottom: '3rem'
+            }}>
+              <div>
+                <h4>Reference Image - {storySet.variant}</h4>
+                <div style={{ width: '100%', height: '100%', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }}>
+                  <Image style={{ width: '100%', objectFit: 'contain' }} {...primaryImage} alt='Mockup Image' />
+                </div>
+              </div>
+              <div>
+                <h4>Developed Component - {storySet.variant}</h4>
+                <div style={{ width: '100%', height: '100%', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }}>
+                  <Story of={storySet.story} />
+                </div>
+              </div>
             </div>
-          </div>
-          <div>
-            <h4>Developed Component</h4>
-            <div style={{ width: '100%', height: '100%', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }}>
-              <Story of={storySet.story} />
-            </div>
-          </div>
-        </div>
-      </>
-      });
+          </>
+        })}
+      </>;
     }
     case 'meta': {
       return <h1>Not defined.</h1>;
--- .storybook/blocks/ReferenceGrid.tsx
+++ .storybook/blocks/ReferenceGrid.tsx
@@ -1,5 +1,3 @@
-import { Box, Button, Typography } from '@mui/material';
-import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 import { useOf } from '@storybook/blocks';
@@ -10,16 +8,20 @@ export default function ReferenceGrid({ of }: ReferenceGridProps) {
     const resolvedOf = useOf(of || 'story', ['story', 'meta']);
     switch (resolvedOf.type) {
         case 'story': {
-            return resolvedOf.story.parameters?.references.map((storySet: any) => {
-            if (!(storySet.images.length > 0)) return null;
             return <>
-                <h3>Reference Images - {storySet.variant}</h3>
-                <div style={{ display: 'grid', gap: '1rem', gridTemplateColumns: '1fr '.repeat(Math.min(storySet.images.length, 4)).trim() }}>
-                    {storySet.images.map(({image}:{image: StaticImageData}, index: number) => {
-                        return <Image key={index} style={{ width: '100%', objectFit: 'contain', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }} {...image} alt='Mockup Image' />
-                    })}
-                </div>
-            </>;})
+                <h3>Reference Image Gallery</h3>
+                {resolvedOf.story.parameters?.references.map((storySet: any) => {
+                    if (!(storySet.images.length > 0)) return null;
+                    return <>
+                        <h3>Reference Images - {storySet.variant} Variant</h3>
+                        <div style={{ display: 'grid', gap: '1rem', gridTemplateColumns: '1fr '.repeat(Math.min(storySet.images.length, 4)).trim() }}>
+                            {storySet.images.map(({ image }: { image: StaticImageData }, index: number) => {
+                                return <Image key={index} style={{ width: '100%', objectFit: 'contain', padding: '0.5rem', border: '2px dashed lightgrey', borderRadius: '0.5rem' }} {...image} alt='Mockup Image' />
+                            })}
+                        </div>
+                    </>;
+                })}
+            </>;
         }
         case 'meta': {
             return <h1>Not defined.</h1>;
