8a1bbc0dc2bac5eba06b133f2e04f0c0d890d709
Josh XT <josh@devxt.com>
2023-10-29 12:15:27 -0400

    Add devops and update packages

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM node:20-alpine AS deps
+WORKDIR /app
+RUN apk add --no-cache libc6-compat
+COPY package.json package-lock.json ./
+RUN --mount=type=cache,target=/var/cache/npm,sharing=locked \
+    npm ci
+
+FROM node:20-alpine AS builder
+WORKDIR /app
+ENV NODE_ENV=production \
+    NEXT_TELEMETRY_DISABLED=1
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+RUN npm run build
+
+FROM node:20-alpine AS runner
+WORKDIR /app
+RUN addgroup -g 1001 nodejs && adduser -D -u 1001 -G nodejs nextjs
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./package.json
+COPY --from=builder /app/package-lock.json ./package-lock.json
+COPY --from=builder /app/public ./public
+COPY --from=builder --chown=nextjs:nodejs /app/.next ./.next
+COPY --from=builder --chown=nextjs:nodejs /app/entrypoint.sh ./entrypoint.sh
+COPY --from=builder --chown=nextjs:nodejs /app/env.sh ./env.sh
+USER nextjs
+EXPOSE 24498
+ENTRYPOINT ["./entrypoint.sh"]
+CMD ["npm", "start"]
---
+++ public/config.js
@@ -0,0 +1,53 @@
+import { isBrowser } from "../lib/utils";
+
+const config = {
+  public: {},
+  serverOnly: {},
+};
+
+const publicPrefix = "NEXT_PUBLIC_";
+const serverOnlyPrefix = "SERVER_ONLY_";
+
+function convertBoolean(value) {
+  if (value === "true") {
+    return true;
+  }
+  if (value === "false") {
+    return false;
+  }
+  return value;
+}
+
+const sortKeys = (obj) => {
+  return Object.keys(obj)
+    .sort()
+    .reduce((acc, key) => {
+      acc[key] = obj[key];
+
+      return acc;
+    }, {});
+};
+
+if (isBrowser()) {
+  Object.entries(window.__env).forEach(([key, value]) => {
+    config.public[key] = convertBoolean(value);
+  });
+  // we need to make sure keys are always in the same order to prevent "Text content did not match" issue
+  // when displaying config on a page
+  config.public = sortKeys(config.public);
+} else {
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (key.startsWith(publicPrefix)) {
+      config.public[key.replace(publicPrefix, "")] = convertBoolean(value);
+    }
+    if (key.startsWith(serverOnlyPrefix)) {
+      config.serverOnly[key.replace(serverOnlyPrefix, "")] =
+        convertBoolean(value);
+    }
+  });
+  // we need to make sure keys are always in the same order to prevent "Text content did not match" issue
+  // when displaying config on a page
+  config.public = sortKeys(config.public);
+}
+
+export default config;
