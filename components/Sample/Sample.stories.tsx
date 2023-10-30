41282ea92e61331523e9b28589f725d848dcc997
James Grieve <jamesonrgrieve@gmail.com>
2023-10-30 17:44:40 -0600

    Fixed spelling error;

--- components/Sample/Sample.stories.tsx
+++ components/Sample/Sample.stories.tsx
@@ -6,12 +6,12 @@ import SampleReferenceImagePrimary from '@/.storybook/ref/Sample/Primary.png';
 import SampleReferenceImageSecondary from '@/.storybook/ref/Sample/Secondary.png';
 
 // Import Component and related types.
-import SampleComponenet, { SampleProps } from './Sample';
+import SampleComponent, { SampleProps } from './Sample';
 
 type Story = StoryObj<typeof meta>;
 
 // Configure Component Stories.
-export const Sample: Story = (args: SampleProps) => <SampleComponenet {...args} />;
+export const Sample: Story = (args: SampleProps) => <SampleComponent {...args} />;
 Sample.args = {
   heading: 'Sample Component',
   bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -21,7 +21,7 @@ Sample.args = {
 // Configure Metadata.
 const meta: Meta = {
   title: 'Sample/Sample',
-  component: SampleComponenet,
+  component: SampleComponent,
   tags: ['autodocs'],
   argTypes: {
     heading: { control: 'text' },
