e4c7dd208476f698d43c5079d95ac2a333cd7299
James Grieve <jamesonrgrieve@gmail.com>
2023-11-02 16:58:13 -0600

    Attempted to implement cookie theming;

--- app/theme.tsx
+++ app/theme.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider, createTheme } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { deepmerge } from '@mui/utils';
 import { ThemeState, ThemeContext } from '@/types/ThemeState';
+import { getCookie, setCookie } from 'cookies-next';
 declare module '@mui/material/styles' {
   interface Palette {
     colorblind: boolean;
@@ -90,27 +91,67 @@ export const themeLight = createTheme(baseTheme);
 export const themeDark = createTheme(deepmerge(baseTheme, darkOverrides));
 export const themeLightColorblind = createTheme(deepmerge(baseTheme, colorblindOverrides));
 export const themeDarkColorblind = createTheme(deepmerge(deepmerge(baseTheme, colorblindOverrides), darkOverrides));
-export function ThemeWrapper({children, defaultDark=false, defaultColorblind=false} : {children: any, defaultDark?: boolean , defaultColorblind?: boolean}) {
+export function ThemeWrapper({ children, defaultDark = false, defaultColorblind = false }: { children: any, defaultDark?: boolean, defaultColorblind?: boolean }) {
+  /*
+  const darkCookie = getCookie("dark", {domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN});
+  const colorblindCookie = getCookie("colorblind", { domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN });
+  console.log(`Retrieved Cookies: dark=${darkCookie}, colorblind=${colorblindCookie}`);
+  console.log(darkCookie==="true");
+  console.log(colorblindCookie==="true");
+  */
   const [themeState, setThemeState] = useState<ThemeState>({
     dark: defaultDark,
     colorblind: defaultColorblind,
     mutate: null
   });
+  /*
+  useEffect(() => {
+    console.log("Loaded");
+    const newDark = getCookie("dark", {
+      domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+    });
+    const newColorblind = getCookie("colorblind", {
+      domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+    });
+    if (newDark !== undefined) setThemeState(old => {
+      return {
+        ...old,
+        dark: newDark === "true"
+      }
+    });
+    if (newColorblind !== undefined) setThemeState(old => {
+      return {
+        ...old,
+        colorblind: newColorblind === "true"
+      }
+    })
 
+  }, [defaultDark, defaultColorblind]);
+  */
   useEffect(() => {
     console.log("Theme State Changed", themeState);
+    setCookie("dark", themeState.dark, {
+      domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+    });
+    setCookie("colorblind", themeState.colorblind, {
+      domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+    });
+    console.log(`Updated Cookies: dark=${getCookie("dark", {
+      domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+    })}, colorblind=${getCookie("colorblind", { domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN })}`);
   }, [themeState])
+  console.log("Using ThemeState", themeState);
   return (
     <ThemeContext.Provider value={{ ...themeState, mutate: setThemeState }}>
-      <ThemeProvider theme={ 
-        !themeState.dark?
-          (!themeState.colorblind?themeLight:themeLightColorblind):
-          (!themeState.colorblind?themeDark:themeDarkColorblind)
-        }>
+      <ThemeProvider theme={
+        !themeState.dark ?
+          (!themeState.colorblind ? themeLight : themeLightColorblind) :
+          (!themeState.colorblind ? themeDark : themeDarkColorblind)
+      }>
         <CssBaseline />
         {children}
       </ThemeProvider>
-      </ThemeContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 export default ThemeWrapper;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_COOKIE_DOMAIN: `.${(process.env.NEXT_PUBLIC_APP_URI??"".split("://")[1])??"".split(":")[0]}`
+    }
+}
 
 module.exports = nextConfig
