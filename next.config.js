016a300f7a774d6577b1806b83f98a36c82ef570
James Grieve <jamesonrgrieve@gmail.com>
2023-11-15 11:53:28 -0700

    Next config update;

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_COOKIE_DOMAIN: `.${(
-      process.env.NEXT_PUBLIC_APP_URI ??
-      ''.split('://')[1] ??
-      ''.split(':')[0]
-    )
-      .split('.')
-      .reverse()
-      .slice(0, 2)
-      .reverse()
-      .join('.')}`
-  }
-};
+    env: {
+        NEXT_PUBLIC_COOKIE_DOMAIN: `.${((process.env.NEXT_PUBLIC_APP_URI??"".split("://")[1])??"".split(":")[0]).split(".").reverse().slice(0, 2).reverse().join(".")}`
+    }
+}
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-});
+    enabled: process.env.NEXT_ANALYZE === 'true'
+  });
 module.exports = withBundleAnalyzer(nextConfig);
