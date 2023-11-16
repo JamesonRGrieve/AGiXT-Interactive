f74349435a6c9f8a32d5c182c432c5677daf832c
James Grieve <jamesonrgrieve@gmail.com>
2023-11-16 12:29:05 -0700

    Added name (reference to NEXT_PUBLIC_APP_NAME);

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box } from '@mui/material';
+import { AppBar, Box, Typography } from '@mui/material';
 import SwitchColorblind from 'jrgcomponents/theming/SwitchColorblind';
 import SwitchDark from 'jrgcomponents/theming/SwitchDark';
 import ConversationSelector from './conversation/ConversationSelector';
@@ -7,14 +7,19 @@ export default function Header({ showConversationSelector }: { showConversationS
   showConversationSelector = true;
   return (
     <AppBar
-      sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', position: 'static', padding: '0.75rem' }}
+      sx={{ display: 'flex', flexDirection: 'row', alignItems:"center", justifyContent: 'space-between', position: 'static', padding: '0.75rem' }}
     >
-      <Box display='flex' flexDirection='row'>
+      <Box display='flex' flex="1" flexDirection='row'>
+      <Box display="flex" flexDirection="row">
         {showConversationSelector ? <ConversationSelector /> : <span>&nbsp;</span>}
+        </Box>
       </Box>
-      <Box>
+      <Typography variant='h1' sx={{justifySelf: "center"}}>{process.env.NEXT_PUBLIC_APP_NAME} Chat</Typography>
+      <Box flex="1">
+        <Box display="flex" justifyContent="end">
         <SwitchDark />
         <SwitchColorblind />
+        </Box>
       </Box>
     </AppBar>
   );
