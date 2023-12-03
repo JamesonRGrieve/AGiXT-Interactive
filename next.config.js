87a2ff49c749763f52270cea73b6f2321094dba5
James Grieve <jamesonrgrieve@gmail.com>
2023-12-02 18:04:38 -0700

    Updated next config;

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_COOKIE_DOMAIN: `.${((process.env.NEXT_PUBLIC_APP_URI ?? '').split('://')[1] ?? '')
+    NEXT_PUBLIC_APP_URI: process.env.APP_URI ?? 'http://localhost:3000',
+    NEXT_PUBLIC_COOKIE_DOMAIN: `.${((process.env.APP_URI ?? 'http://localhost:3000').split('://')[1] ?? '')
       .split(':')[0]
       .split('.')
       .reverse()
