c38b7a88f42cb92c40940ad16db6e026c08a5dc8
James Grieve <jamesonrgrieve@gmail.com>
2023-12-20 17:01:02 -0700

    Removed NEXT_PUBLIC from env inputs;

--- next.config.js
+++ next.config.js
@@ -2,29 +2,22 @@
 const nextConfig = {
   env: {
     // Options
-    NEXT_PUBLIC_AGIXT_SERVER: process.env.NEXT_PUBLIC_AGIXT_SERVER || process.env.AGIXT_SERVER || 'http://localhost:7437',
-    NEXT_PUBLIC_AGIXT_AGENT: process.env.NEXT_PUBLIC_AGIXT_AGENT || process.env.AGIXT_AGENT || 'gpt4free',
-    NEXT_PUBLIC_AGIXT_INSIGHT_AGENT:
-      process.env.NEXT_PUBLIC_AGIXT_INSIGHT_AGENT || process.env.AGIXT_INSIGHT_AGENT || 'gpt4free',
-    NEXT_PUBLIC_AGIXT_MODE: process.env.NEXT_PUBLIC_AGIXT_MODE || process.env.AGIXT_MODE || 'prompt',
+    NEXT_PUBLIC_AGIXT_SERVER: process.env.AGIXT_SERVER || 'http://localhost:7437',
+    NEXT_PUBLIC_AGIXT_AGENT: process.env.AGIXT_AGENT || 'gpt4free',
+    NEXT_PUBLIC_AGIXT_INSIGHT_AGENT: process.env.AGIXT_INSIGHT_AGENT || 'gpt4free',
+    NEXT_PUBLIC_AGIXT_MODE: process.env.AGIXT_MODE || 'prompt',
     // Prompt Mode Options
-    NEXT_PUBLIC_AGIXT_PROMPT_NAME: process.env.NEXT_PUBLIC_AGIXT_PROMPT_NAME || process.env.AGIXT_PROMPT_NAME || 'Chat',
-    NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY:
-      process.env.NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY || process.env.AGIXT_PROMPT_CATEGORY || 'Default',
+    NEXT_PUBLIC_AGIXT_PROMPT_NAME:  process.env.AGIXT_PROMPT_NAME || 'Chat',
+    NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY: process.env.AGIXT_PROMPT_CATEGORY || 'Default',
     // Chain Mode Options
-    NEXT_PUBLIC_AGIXT_CHAIN: process.env.NEXT_PUBLIC_AGIXT_CHAIN || process.env.AGIXT_CHAIN || 'Postgres Chat',
-    NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT:
-      process.env.NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT || process.env.AGIXT_USE_SELECTED_AGENT || 'true',
-    NEXT_PUBLIC_AGIXT_CHAIN_ARGS: process.env.NEXT_PUBLIC_AGIXT_CHAIN_ARGS || process.env.AGIXT_CHAIN_ARGS || '{}',
+    NEXT_PUBLIC_AGIXT_CHAIN: process.env.AGIXT_CHAIN || 'Postgres Chat',
+    NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT: process.env.AGIXT_USE_SELECTED_AGENT || 'true',
+    NEXT_PUBLIC_AGIXT_CHAIN_ARGS: process.env.AGIXT_CHAIN_ARGS || '{}',
     // UI Options
-    NEXT_PUBLIC_AGIXT_FILE_UPLOAD_ENABLED:
-      process.env.NEXT_PUBLIC_AGIXT_FILE_UPLOAD_ENABLED || process.env.AGIXT_FILE_UPLOAD_ENABLED || 'false',
-    NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR:
-      process.env.NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR || process.env.AGIXT_AGIXT_SHOW_CONVERSATION_BAR || 'true',
-    NEXT_PUBLIC_AGIXT_SHOW_APP_BAR:
-      process.env.NEXT_PUBLIC_AGIXT_SHOW_APP_BAR || process.env.AGIXT_AGIXT_SHOW_APP_BAR || 'true',
-    NEXT_PUBLIC_AGIXT_CONVERSATION_NAME:
-      process.env.NEXT_PUBLIC_AGIXT_CONVERSATION_NAME || process.env.AGIXT_CONVERSATION_NAME || 'AGiXT Conversation',
+    NEXT_PUBLIC_AGIXT_FILE_UPLOAD_ENABLED: process.env.AGIXT_FILE_UPLOAD_ENABLED || 'false',
+    NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR: process.env.AGIXT_AGIXT_SHOW_CONVERSATION_BAR || 'true',
+    NEXT_PUBLIC_AGIXT_SHOW_APP_BAR: process.env.AGIXT_AGIXT_SHOW_APP_BAR || 'true',
+    NEXT_PUBLIC_AGIXT_CONVERSATION_NAME: process.env.AGIXT_CONVERSATION_NAME || 'AGiXT Conversation',
     NEXT_PUBLIC_APP_URI: process.env.APP_URI ?? 'http://localhost:3100',
     NEXT_PUBLIC_COOKIE_DOMAIN: `.${((process.env.APP_URI ?? 'http://localhost:3100').split('://')[1] ?? '')
       .split(':')[0]
