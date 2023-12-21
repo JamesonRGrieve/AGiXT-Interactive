2d28abc614f8af63068d96cd759cf52fd85a1910
James Grieve <jamesonrgrieve@gmail.com>
2023-12-21 12:21:18 -0700

    Removed analyze; Added SEO metadata;

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,18 +11,27 @@ import SwitchDark from 'jrgcomponents/theming/SwitchDark';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'JRG Next.js Boilerplate',
-  description: 'Boilerplate for Next.JS Applications'
+  title: process.env.NEXT_PUBLIC_APP_NAME,
+  description: process.env.NEXT_PUBLIC_APP_DESCRIPTION
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const cookieStore = cookies();
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <meta name='google-adsense-account' content={process.env.NEXT_PUBLIC_ADSENSE_ACCOUNT} />
+        <meta property='og:url' content={process.env.NEXT_PUBLIC_APP_URI} />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={process.env.NEXT_PUBLIC_APP_NAME} />
+        <meta property='og:description' content={process.env.NEXT_PUBLIC_APP_DESCRIPTION} />
+        <meta property='og:image' content={process.env.NEXT_PUBLIC_APP_LOGO_URI || `${process.env.NEXT_PUBLIC_APP_URI}/favicon.ico`} />
+      </head>
       <body className={inter.className}>
         <ThemeWrapper
           themes={themes}
-          defaultDark={cookieStore.get('dark')?.value === 'true'}
+          defaultDark={cookieStore.get('dark')?.value ? cookieStore.get('dark')?.value === 'true' : process.env.NEXT_PUBLIC_DEFAULT_THEME_MODE === 'dark' }
           defaultColorblind={cookieStore.get('colorblind')?.value === 'true'}
         >
           <AppBar
--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_APP_URI: process.env.APP_URI ?? 'http://localhost:3000',
+    NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || 'AGiXT',
+    NEXT_PUBLIC_APP_DESCRIPTION: process.env.APP_DESCRIPTION || 'An AGiXT application.',
+    NEXT_PUBLIC_APP_LOGO_URI: process.env.APP_LOGO || '',
+    NEXT_PUBLIC_THEME_NAME: process.env.THEME_NAME || '',
+    NEXT_PUBLIC_DEFAULT_THEME_MODE: process.env.DEFAULT_THEME_MODE || 'light',
+    NEXT_PUBLIC_ADSENSE_ACCOUNT: process.env.ADSENSE_ACCOUNT || '',
     NEXT_PUBLIC_COOKIE_DOMAIN: `.${((process.env.APP_URI ?? 'http://localhost:3000').split('://')[1] ?? '')
       .split(':')[0]
       .split('.')
