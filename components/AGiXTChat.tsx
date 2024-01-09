f991eaf8f1a78ef0e9e2159d1e3ba9947746d56c
James Grieve <jamesonrgrieve@gmail.com>
2024-01-09 16:24:23 -0700

    Seems to work;

--- components/AGiXTChat.tsx
+++ components/AGiXTChat.tsx
@@ -19,12 +19,12 @@ const Stateful = (props: ChatProps) => {
   return (
     <ContextWrapper
       requireKey={process.env.NEXT_PUBLIC_AGIXT_REQUIRE_API_KEY === 'true'}
-      apiKey={props.opts.apiKey || process.env.NEXT_PUBLIC_API_KEY || ''}
-      agixtServer={props.opts.agixtServer || process.env.NEXT_PUBLIC_AGIXT_SERVER || 'http://localhost:7437'}
+      apiKey={props.opts?.apiKey || process.env.NEXT_PUBLIC_API_KEY || ''}
+      agixtServer={props.opts?.agixtServer || process.env.NEXT_PUBLIC_AGIXT_SERVER || 'http://localhost:7437'}
       initialState={{
         ...ChatDefaultState,
-        prompt: props.opts.promptName || process.env.NEXT_PUBLIC_AGIXT_PROMPT_NAME,
-        promptCategory: props.opts.promptCategory || process.env.NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY
+        prompt: props.opts?.promptName || process.env.NEXT_PUBLIC_AGIXT_PROMPT_NAME,
+        promptCategory: props.opts?.promptCategory || process.env.NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY
       }}>
       <Chat {...props} />
     </ContextWrapper>
