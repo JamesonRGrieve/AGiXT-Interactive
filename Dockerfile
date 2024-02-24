26d02151f4361f03adf616c29fdbef9a3e7ee176
Josh XT <josh@devxt.com>
2024-02-24 06:21:49 -0500

    use .env

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:20-alpine
 WORKDIR /app
-RUN --mount=type=cache,target=/var/cache/apk,sharing=locked \
-    apk add --no-cache libc6-compat git
+RUN apk add --no-cache libc6-compat git
 COPY . .
 RUN chmod +x /app/launch.sh
 EXPOSE 3437
--- launch.sh
+++ launch.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-echo "" > /app/.env.local
+echo "" > /app/.env
 env | while IFS='=' read -r name value; do
-  printf '%s="%s"\n' "$name" "$value" >> /app/.env.local
+  printf '%s="%s"\n' "$name" "$value" >> /app/.env
 done
 # Show full env
-cat /app/.env.local
+cat /app/.env
 git clone https://github.com/JamesonRGrieve/jrgcomponents-themes /app/themes
 set -a
-. /app/.env.local
+. /app/.env
 set +a
-theme=$(grep '^THEME_NAME=' /app/.env.local | cut -d'=' -f2)
+theme=$(grep '^THEME_NAME=' /app/.env | cut -d'=' -f2)
 cp -r /app/themes/$theme/* /app
 npm install -g npm@latest
 npm install
 npm run build
-rm /app/.env.local
+rm /app/.env
 npm start
