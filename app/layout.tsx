85eaaae0941d9183a6d83a74002c579137dbaf77
James Grieve <jamesonrgrieve@gmail.com>
2024-03-20 13:13:15 -0600

    Updates;

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { cookies } from 'next/headers';
 // eslint-disable-next-line import/no-unassigned-import
-import './globals.css';
+import 'jrgcomponents/Style/Global';
 import ThemeWrapper from 'jrgcomponents/Theming/ThemeWrapper';
 import theme from './theme';
+import Head from 'jrgcomponents/Head';
 import React, { ReactNode } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,18 +19,7 @@ export default function RootLayout({ children }: { children: ReactNode }): React
 
   return (
     <html lang='en'>
-      <head>
-        <link rel='icon' href='/favicon.ico' sizes='any' />
-        <meta name='google-adsense-account' content={process.env.NEXT_PUBLIC_ADSENSE_ACCOUNT} />
-        <meta property='og:url' content={process.env.NEXT_PUBLIC_APP_URI} />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content={process.env.NEXT_PUBLIC_APP_NAME} />
-        <meta property='og:description' content={process.env.NEXT_PUBLIC_APP_DESCRIPTION} />
-        <meta
-          property='og:image'
-          content={process.env.NEXT_PUBLIC_APP_LOGO_URI || `${process.env.NEXT_PUBLIC_APP_URI}/favicon.ico`}
-        />
-      </head>
+      <Head />
       <body className={inter.className}>
         <ThemeWrapper
           themeInjection={{ theme: theme }}
