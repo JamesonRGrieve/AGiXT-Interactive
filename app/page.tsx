d2bba4912d6924814a488570d0a4837082ea75fc
James Grieve <jamesonrgrieve@gmail.com>
2024-04-03 10:48:50 -0600

    Move app bar stuff into app and out of the interactive component itself;

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import AGiXTChat from '../components/InteractiveAGiXT';
 import { cookies } from 'next/headers';
 import Login from './Login';
+import ConversationSelector from '../components/ConversationSelector';
+import AppWrapper from 'jrgcomponents/AppWrapper/Wrapper';
+import { Typography } from '@mui/material';
 export default function Home() {
   const cookieStore = cookies();
   const apiKey =
@@ -10,24 +13,50 @@ export default function Home() {
   return process.env.NEXT_PUBLIC_AGIXT_REQUIRE_API_KEY === 'true' && !apiKey ? (
     <Login />
   ) : (
-    <AGiXTChat
-      uiConfig={{
-        showAppBar: process.env.NEXT_PUBLIC_AGIXT_SHOW_APP_BAR === 'true', // Show the conversation selection bar to create, delete, and export conversations
-        showConversationSelector: process.env.NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR === 'true', // Show the conversation selection bar to create, delete, and export conversations
-        showRLHF: process.env.NEXT_PUBLIC_AGIXT_RLHF === 'true',
-        showChatThemeToggles: process.env.NEXT_PUBLIC_AGIXT_SHOW_CHAT_THEME_TOGGLES === 'true',
-        footerMessage: process.env.NEXT_PUBLIC_AGIXT_FOOTER_MESSAGE || 'Powered by AGiXT',
-        alternateBackground: 'primary',
-      }}
-      chatConfig={{
-        mode: (process.env.NEXT_PUBLIC_AGIXT_MODE && ['chain', 'prompt'].includes(process.env.NEXT_PUBLIC_AGIXT_MODE)
-          ? process.env.NEXT_PUBLIC_AGIXT_MODE
-          : 'prompt') as 'chain' | 'prompt',
-      }}
-      serverConfig={{
-        apiKey: apiKey,
-        agixtServer: process.env.NEXT_PUBLIC_AGIXT_SERVER ?? '',
-      }}
-    />
+    <AppWrapper
+      header={
+        process.env.NEXT_PUBLIC_AGIXT_SHOW_APP_BAR === 'true' && {
+          components: {
+            left:
+              process.env.NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR === 'true' ? (
+                <ConversationSelector />
+              ) : (
+                <span>&nbsp;</span>
+              ),
+          },
+        }
+      }
+      footer={
+        process.env.NEXT_PUBLIC_AGIXT_FOOTER_MESSAGE && {
+          components: {
+            center: (
+              <Typography sx={{ margin: 0 }} variant='caption' textAlign='center'>
+                {process.env.NEXT_PUBLIC_AGIXT_FOOTER_MESSAGE}
+              </Typography>
+            ),
+          },
+        }
+      }
+    >
+      <AGiXTChat
+        uiConfig={{
+          showAppBar: process.env.NEXT_PUBLIC_AGIXT_SHOW_APP_BAR === 'true', // Show the conversation selection bar to create, delete, and export conversations
+          showConversationSelector: process.env.NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR === 'true', // Show the conversation selection bar to create, delete, and export conversations
+          showRLHF: process.env.NEXT_PUBLIC_AGIXT_RLHF === 'true',
+          showChatThemeToggles: process.env.NEXT_PUBLIC_AGIXT_SHOW_CHAT_THEME_TOGGLES === 'true',
+          footerMessage: process.env.NEXT_PUBLIC_AGIXT_FOOTER_MESSAGE || 'Powered by AGiXT',
+          alternateBackground: 'primary',
+        }}
+        chatConfig={{
+          mode: (process.env.NEXT_PUBLIC_AGIXT_MODE && ['chain', 'prompt'].includes(process.env.NEXT_PUBLIC_AGIXT_MODE)
+            ? process.env.NEXT_PUBLIC_AGIXT_MODE
+            : 'prompt') as 'chain' | 'prompt',
+        }}
+        serverConfig={{
+          apiKey: apiKey,
+          agixtServer: process.env.NEXT_PUBLIC_AGIXT_SERVER ?? '',
+        }}
+      />
+    </AppWrapper>
   );
 }
