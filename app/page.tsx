b3e8dfc3593aeb9402538a7ae89adfdb6fd337e1
James Grieve <jamesonrgrieve@gmail.com>
2024-04-08 14:56:57 -0600

    Remove support for 'apiKey' cookie;

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ import AppWrapper from 'jrgcomponents/AppWrapper/Wrapper';
 import { Box, Typography } from '@mui/material';
 export default function Home() {
   const cookieStore = cookies();
-  const apiKey =
-    process.env.NEXT_PUBLIC_AGIXT_API_KEY ?? cookieStore.get('apiKey')?.value ?? cookieStore.get('jwt')?.value ?? '';
+  const apiKey = process.env.NEXT_PUBLIC_AGIXT_API_KEY ?? cookieStore.get('jwt')?.value ?? '';
   // console.log('Server-Side API Key: ', apiKey);
   return process.env.NEXT_PUBLIC_AGIXT_REQUIRE_API_KEY === 'true' && !apiKey ? <Login /> : <AGiXTChat />;
 }
