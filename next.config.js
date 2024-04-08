c46fd8091e11eeec40104e8d941b07fac51b7f80
James Grieve <jamesonrgrieve@gmail.com>
2024-04-07 20:05:21 -0600

    Auth on by default;

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ const nextConfig = {
     // Command Mode Options, Defined in ./types/ChatDefault.js
     NEXT_PUBLIC_AGIXT_COMMAND: process.env.AGIXT_COMMAND || ChatDefault.command,
     NEXT_PUBLIC_AGIXT_COMMAND_MESSAGE_ARG: process.env.AGIXT_COMMAND_MESSAGE_ARG || ChatDefault.commandMessageArg,
-    NEXT_PUBLIC_APP_AUTHENTICATION_ENABLED: process.env.APP_AUTHENTICATION_ENABLED || 'false',
+    NEXT_PUBLIC_APP_AUTHENTICATION_ENABLED: process.env.APP_AUTHENTICATION_ENABLED || 'true',
 
     NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT:
       process.env.AGIXT_USE_SELECTED_AGENT || String(ChatDefault.chatSettings.chainRunConfig.useSelectedAgent),
