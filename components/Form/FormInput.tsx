8e69324c262044d3a0ce54f515f953d496c49876
James Grieve <jamesonrgrieve@gmail.com>
2024-04-09 14:52:20 -0600

    Remove double selector;

--- components/Form/FormInput.tsx
+++ components/Form/FormInput.tsx
@@ -7,9 +7,6 @@ import ConversationSelector from '../Selectors/ConversationSelector';
 export default function FormInput({ argValues, setArgValues, disabled }) {
   return (
     <Box mt='1rem' px='1rem' display='flex' flexDirection='row' gap='1rem' justifyContent='center' alignItems='center'>
-      {process.env.NEXT_PUBLIC_AGIXT_SHOW_SELECTION_BAR === 'agent' && <AgentSelector />}
-      {process.env.NEXT_PUBLIC_AGIXT_SHOW_SELECTION_BAR === 'prompt' && <PromptSelector />}
-      {process.env.NEXT_PUBLIC_AGIXT_SHOW_SELECTION_BAR === 'conversation' && <ConversationSelector />}
       {Object.keys(argValues).map((arg) => {
         const [argType, argName, ...argConfig] = arg.split('_');
         if (argType.toLowerCase() === 'text')
