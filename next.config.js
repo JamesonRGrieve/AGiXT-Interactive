ab25f5d7c70bf37b4834f3583d6103baf30ad343
James Grieve <jamesonrgrieve@gmail.com>
2024-04-10 09:31:03 -0600

    Update next config;

--- next.config.js
+++ next.config.js
@@ -5,42 +5,49 @@ const nextConfig = {
   env: {
     // App Options
     NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || 'AGiXT',
-    NEXT_PUBLIC_APP_URI: process.env.APP_URI ?? 'http://localhost:3100',
     NEXT_PUBLIC_APP_DESCRIPTION: process.env.APP_DESCRIPTION || 'An AGiXT application.',
-    NEXT_PUBLIC_DEFAULT_THEME_MODE: process.env.DEFAULT_THEME_MODE || 'dark',
+    NEXT_PUBLIC_APP_URI: process.env.APP_URI ?? 'http://localhost:3100',
+    NEXT_PUBLIC_THEME_DEFAULT_MODE: process.env.DEFAULT_THEME_MODE || 'dark',
+    NEXT_PUBLIC_TZ: process.env.TZ || 'TZ-America/New_York', // Server timezone
+    NEXT_PUBLIC_AUTH_WEB: process.env.AUTH_WEB || undefined, // undefined to disable auth.
+
     // Monetization Options
     NEXT_PUBLIC_ADSENSE_ACCOUNT: process.env.ADSENSE_ACCOUNT || '',
-    // Options
+
+    // Common Options
+    NEXT_PUBLIC_AGIXT_CONVERSATION_NAME: process.env.AGIXT_CONVERSATION || 'Default',
     NEXT_PUBLIC_AGIXT_CONVERSATION_MODE: process.env.AGIXT_CONVERSATION_MODE || 'static', // static, select or uuid
-    NEXT_PUBLIC_AGIXT_CONVERSATION_NAME: process.env.AGIXT_CONVERSATION_NAME || 'Default',
-    NEXT_PUBLIC_AGIXT_ENABLE_SEARCHPARAM_CONFIG: process.env.AGIXT_ENABLE_SEARCHPARAM_CONFIG || 'true',
-    NEXT_PUBLIC_AGIXT_MODE: process.env.AGIXT_MODE || 'prompt',
-    NEXT_PUBLIC_AGIXT_REQUIRE_API_KEY: process.env.AGIXT_REQUIRE_API_KEY || 'true',
-    NEXT_PUBLIC_AGIXT_API_KEY: process.env.AGIXT_API_KEY || '',
+    NEXT_PUBLIC_AGIXT_API_KEY: process.env.AGIXT_API_KEY || undefined, // undefined to prompt, empty string for cookie, or defined for static
     NEXT_PUBLIC_AGIXT_SERVER: process.env.AGIXT_SERVER || 'http://localhost:7437',
-    NEXT_PUBLIC_INTERACTIVE_MODE: process.env.INTERACTIVE_MODE || 'chat',
+
     // UI Options
+    NEXT_PUBLIC_INTERACTIVE_UI: process.env.INTERACTIVE_MODE || 'chat',
+    NEXT_PUBLIC_AGIXT_SHOW_APP_BAR: process.env.AGIXT_SHOW_APP_BAR || 'true',
+    NEXT_PUBLIC_AGIXT_SHOW_SELECTION: process.env.AGIXT_SHOW_SELECTION_BAR || '', // empty, 'agent', 'conversation' or 'prompt'
     NEXT_PUBLIC_AGIXT_FOOTER_MESSAGE: process.env.AGIXT_FOOTER_MESSAGE || 'Powered by AGiXT',
     NEXT_PUBLIC_AGIXT_RLHF: process.env.AGIXT_RLHF || 'false',
-    NEXT_PUBLIC_AGIXT_SHOW_APP_BAR: process.env.AGIXT_SHOW_APP_BAR || 'true',
     NEXT_PUBLIC_AGIXT_SHOW_CHAT_THEME_TOGGLES: process.env.AGIXT_SHOW_CHAT_THEME_TOGGLES || 'false',
-    NEXT_PUBLIC_AGIXT_SHOW_CONVERSATION_BAR: process.env.AGIXT_SHOW_CONVERSATION_BAR || 'false',
+    NEXT_PUBLIC_AGIXT_FILE_UPLOAD_ENABLED:
+      process.env.AGIXT_FILE_UPLOAD_ENABLED || String(ChatDefault.chatSettings.enableFileUpload),
+    NEXT_PUBLIC_AGIXT_VOICE_INPUT_ENABLED:
+      process.env.AGIXT_VOICE_INPUT_ENABLED || String(ChatDefault.chatSettings.enableVoiceInput),
 
+    // Override Options
+    NEXT_PUBLIC_AGIXT_ENABLE_SEARCHPARAM_CONFIG: process.env.AGIXT_ENABLE_SEARCHPARAM_CONFIG || 'true',
+    NEXT_PUBLIC_AGIXT_MODE: process.env.AGIXT_MODE || 'prompt',
     // State Options, Defined in ./types/ChatDefault.js
     NEXT_PUBLIC_AGIXT_AGENT: process.env.AGIXT_AGENT || ChatDefault.chatSettings.selectedAgent,
     NEXT_PUBLIC_AGIXT_INSIGHT_AGENT: process.env.AGIXT_INSIGHT_AGENT || ChatDefault.chatSettings.insightAgentName,
-    NEXT_PUBLIC_AGIXT_FILE_UPLOAD_ENABLED:
-      process.env.AGIXT_FILE_UPLOAD_ENABLED || String(ChatDefault.chatSettings.enableFileUpload),
+    NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT:
+      process.env.AGIXT_USE_SELECTED_AGENT || String(ChatDefault.chatSettings.chainRunConfig.useSelectedAgent),
     // Prompt Mode Options, Defined in ./types/ChatDefault.js
     NEXT_PUBLIC_AGIXT_PROMPT_NAME: process.env.AGIXT_PROMPT_NAME || ChatDefault.prompt,
     NEXT_PUBLIC_AGIXT_PROMPT_CATEGORY: process.env.AGIXT_PROMPT_CATEGORY || ChatDefault.promptCategory,
-    // Chain Mode Options, Defined in ./types/ChatDefault.js
-    NEXT_PUBLIC_AGIXT_CHAIN: process.env.AGIXT_CHAIN || ChatDefault.chain,
+    // Command Mode Options, Defined in ./types/ChatDefault.js
     NEXT_PUBLIC_AGIXT_COMMAND: process.env.AGIXT_COMMAND || ChatDefault.command,
     NEXT_PUBLIC_AGIXT_COMMAND_MESSAGE_ARG: process.env.AGIXT_COMMAND_MESSAGE_ARG || ChatDefault.commandMessageArg,
-
-    NEXT_PUBLIC_AGIXT_USE_SELECTED_AGENT:
-      process.env.AGIXT_USE_SELECTED_AGENT || String(ChatDefault.chatSettings.chainRunConfig.useSelectedAgent),
+    // Chain Mode Options, Defined in ./types/ChatDefault.js
+    NEXT_PUBLIC_AGIXT_CHAIN: process.env.AGIXT_CHAIN || ChatDefault.chain,
     NEXT_PUBLIC_AGIXT_CHAIN_ARGS:
       process.env.AGIXT_CHAIN_ARGS || JSON.stringify(ChatDefault.chatSettings.chainRunConfig.chainArgs),
 
@@ -52,6 +59,15 @@ const nextConfig = {
       .slice(0, 2)
       .reverse()
       .join('.')}`,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: (process.env.MODE ?? 'production').toLowerCase() !== 'development',
+  },
+  typescript: {
+    // Warning: Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: (process.env.MODE ?? 'production').toLowerCase() !== 'development',
   },
 };
 // eslint-disable-next-line @typescript-eslint/no-var-requires
