263b0cddb0f9493a0d8010832bd2f8aa1b70cf75
James Grieve <jamesonrgrieve@gmail.com>
2024-04-10 10:14:08 -0600

    Change file name;

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import AGiXTChat from '../components/InteractiveAGiXT';
+import InteractiveAGiXT from '../components/InteractiveAGiXT';
 import { cookies } from 'next/headers';
-import EnterKey from './EnterKey';
+import EnterAPIKey from './EnterAPIKey';
 export default function Home() {
   const apiKey = process.env.NEXT_PUBLIC_AGIXT_API_KEY ?? cookies().get('jwt')?.value ?? '';
   if (process.env.MODE === 'development') {
     console.log('Server-Side API Key: ', apiKey);
   }
-  return apiKey ? <AGiXTChat /> : <EnterKey />;
+  return apiKey ? <InteractiveAGiXT /> : <EnterAPIKey />;
 }
