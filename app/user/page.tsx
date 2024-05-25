463bab0398a4dd72d25809003f6f55d7d9fc09c4
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-05-24 21:02:06 -0400

    Add Magical Auth (#42)
    * Add middleware and user pages;
    
    * Updated deps; Confirmed build;
    
    * Apply updates to user system;
    
    * Fix env config;
    
    ---------
    
    Co-authored-by: James Grieve <jamesonrgrieve@gmail.com>

---
+++ app/user/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { Box, Button, TextField } from '@mui/material';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, ReactNode } from 'react';
+import { setCookie } from 'cookies-next';
+export default function Identify(): ReactNode {
+  const router = useRouter();
+  const submitForm = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData((event.currentTarget as HTMLFormElement) ?? undefined));
+    const existsResponse = (
+      await axios
+        .get(`${process.env.NEXT_PUBLIC_NOTES_SERVER}/v1/user/exists?email=${formData.email.toString()}`)
+        .catch((exception: any) => exception.response)
+    ).data;
+    console.log(existsResponse);
+    try {
+      setCookie('email', formData.email.toString(), { domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN });
+      router.push(existsResponse ? '/user/login' : '/user/register');
+    } catch (exception) {
+      console.error(exception);
+    }
+  };
+
+  return (
+    <Box component='form' onSubmit={submitForm} display='flex' flexDirection='column' gap='1rem'>
+      <TextField id='email' label='E-Mail Address' variant='outlined' name='email' />
+      <Button type='submit'>Continue</Button>
+    </Box>
+  );
+}
