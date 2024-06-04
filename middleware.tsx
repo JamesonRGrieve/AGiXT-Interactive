6d36fcb3fe6541beae355302a44a9578eb437964
James Grieve <jamesonrgrieve@gmail.com>
2024-06-04 10:08:47 -0600

    Seems to work;

--- middleware.tsx
+++ middleware.tsx
@@ -1,13 +1,38 @@
 import { NextResponse, NextRequest } from 'next/server';
-export default async function Middleware(req: NextRequest) {
+
+const AuthMode = {
+  None: 0,
+  GTAuth: 1,
+  MagicalAuth: 2,
+};
+const generateCookieString = (key: string, value: string, age: string): string =>
+  `${key}=${value}; Domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; Path=/; Max-Age=${age}; Same-Site: strict;`;
+export default async function Middleware(req: NextRequest): Promise<NextResponse> {
   if (
-    process.env.AUTH_WEB?.startsWith(process.env.APP_URI ?? 'App URI Not Found') &&
-    !process.env.AUTH_WEB.endsWith('/user')
+    req.nextUrl.pathname.startsWith('/_next/') ||
+    req.nextUrl.pathname.startsWith('/api/') ||
+    req.nextUrl.pathname === '/favicon.ico'
   ) {
-    throw new Error('Invalid AUTH_WEB. For Magical Auth implementations, AUTH_WEB must point to /user.');
+    return NextResponse.next();
   }
-  const reqURI = req.nextUrl.href.replace('https://localhost:3437', process.env.APP_URI);
-  const headers: any = {};
+
+  let authMode = AuthMode.None;
+  if (process.env.AUTH_WEB && process.env.AUTH_SERVER) {
+    if (process.env.APP_URI && process.env.AUTH_WEB.startsWith(process.env.APP_URI)) {
+      authMode = AuthMode.MagicalAuth;
+      if (!process.env.AUTH_WEB.endsWith('/user')) {
+        throw new Error('Invalid AUTH_WEB. For Magical Auth implementations, AUTH_WEB must point to APP_URI/user.');
+      }
+    } else {
+      authMode = AuthMode.GTAuth;
+    }
+  }
+  console.log('Authentication Mode:', authMode);
+  console.log(req);
+  const requestedURI = req.url.replace('localhost', '127.0.0.1').split('?')[0];
+  console.log('Requested URI:', requestedURI);
+  const headers: HeadersInit = {};
+  // Middleware doesn't have a great method for pulling query parameters (yet).
   const queryParams = req.url.includes('?')
     ? Object.assign(
         {},
@@ -17,62 +42,61 @@ export default async function Middleware(req: NextRequest) {
           .map((param) => ({ [param.split('=')[0]]: param.split('=')[1] })),
       )
     : {};
-  console.log(queryParams);
-  if (queryParams.token) {
-    console.log('Line 22');
-    headers['Set-Cookie'] =
-      `jwt=${queryParams.token}; Domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; Path=/; SameSite=Strict;`;
-    const urlWithoutParams = req.url.split('?')[0];
-    return NextResponse.redirect(urlWithoutParams, { headers });
+  console.log('Query Parameters:', queryParams);
+  if (queryParams.token || queryParams.jwt) {
+    // This should set the cookie and then re-run the middleware (without query params).
+    return NextResponse.redirect(req.cookies.get('href')?.value ?? requestedURI, {
+      headers: {
+        // @ts-expect-error NextJS' types are wrong.
+        'Set-Cookie': [
+          generateCookieString('jwt', queryParams.token ?? queryParams.jwt, (86400 * 7).toString()),
+          generateCookieString('href', '', (0).toString()),
+        ],
+      },
+    });
   }
-  if (req.nextUrl.pathname.startsWith('/_next/') || req.nextUrl.pathname === '/favicon.ico') {
-    return NextResponse.next();
-  } else if (process.env.AUTH_WEB) {
-    console.log('Line 31');
-    let jwt = req.cookies.get('jwt')?.value;
+  if (authMode) {
+    const rawJWT = req.cookies.get('jwt')?.value;
+    // Strip any and all 'Bearer 's off of JWT.
+    const jwt = rawJWT?.split(' ')[rawJWT?.split(' ').length - 1] ?? rawJWT ?? '';
+    console.log('JWT:', jwt);
     if (jwt) {
-      console.log('Line 34');
       try {
-        console.log('Sending', `${jwt.startsWith('Bearer ') ? jwt : 'Bearer ' + jwt} to ${process.env.AUTH_SERVER}/v1/user`);
-        const response = await fetch(`${process.env.AUTH_SERVER}/v1/user`, {
+        const authEndpoint = `${process.env.AUTH_SERVER}/v1/user`;
+        console.log(`Verifying JWT Bearer ${jwt} with AUTH_SERVER at ${authEndpoint}...`);
+        const response = await fetch(authEndpoint, {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: jwt,
+            Authorization: `${jwt}`,
           },
         });
-        if (response.status !== 200) throw new Error('Invalid token response, status ' + response.status + '.');
+        if (response.status !== 200) {
+          throw new Error(`Invalid token response, status ${response.status}, detail ${(await response.json()).detail}.`);
+        }
       } catch (exception) {
-        console.log('Line 45');
-        console.error('Invalid token. Logging out.', exception);
-        headers['Set-Cookie'] = `jwt=; Domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; Path=/; SameSite=Strict; Max-Age=0;`;
-        jwt = '';
-        return NextResponse.redirect(new URL(process.env.AUTH_WEB), { headers });
-      }
-    }
-    if (reqURI.startsWith(process.env.AUTH_WEB)) {
-      if (jwt && req.nextUrl.pathname !== '/user/manage') {
-        console.log('Line 54');
-        return NextResponse.redirect(new URL(process.env.AUTH_WEB + '/manage'));
-      } else {
-        console.log('Line 57');
-        return NextResponse.next();
+        console.error(`Invalid token. Logging out and redirecting to AUTH_WEB at ${process.env.AUTH_WEB}.`, exception);
+        const response = NextResponse.redirect(new URL(process.env.AUTH_WEB), { headers });
+        // @ts-expect-error NextJS' types are wrong.
+        response.headers.set('Set-Cookie', [
+          generateCookieString('jwt', '', (0).toString()),
+          generateCookieString('href', requestedURI, (86400).toString()),
+        ]);
+        return response;
       }
     } else {
-      console.log('Line 61');
-      if (!jwt) {
-        console.log('Line 63');
-        console.log(`${reqURI} does not start with ${process.env.AUTH_WEB} and no valid JWT, redirecting...`);
-        headers['Set-Cookie'] =
-          `href=${reqURI}; Domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; Path=/; SameSite=Strict; Max-Age=0;`;
-        console.log('href cookie set to: ', reqURI);
-        return NextResponse.redirect(new URL(process.env.AUTH_WEB), { headers });
+      if (authMode === AuthMode.MagicalAuth && requestedURI.startsWith(process.env.AUTH_WEB)) {
+        // Don't let users visit Identify, Register or Login pages if they're already logged in.
+        if (jwt.length > 0 && req.nextUrl.pathname !== '/user/manage') {
+          return NextResponse.redirect(new URL(process.env.AUTH_WEB + '/manage'));
+        } else {
+          return NextResponse.next();
+        }
+      } else {
+        return NextResponse.redirect(new URL(process.env.AUTH_WEB), {
+          headers: { 'Set-Cookie': generateCookieString('href', requestedURI, (86400).toString()) },
+        });
       }
     }
-  } else if (req.nextUrl.pathname.startsWith('/api/')) {
-    return NextResponse.next();
-  } else if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) {
-    return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
-  } else {
     return NextResponse.next();
   }
 }
