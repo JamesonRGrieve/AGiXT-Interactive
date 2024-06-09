93cc472f102cbb872cc65b5be29874bab5297f23
James Grieve <jamesonrgrieve@gmail.com>
2024-06-09 12:40:22 -0600

    Add filtering to insights;

--- components/DataGridFromCSV.tsx
+++ components/DataGridFromCSV.tsx
@@ -8,6 +8,7 @@ import { InteractiveConfig } from '@/types/InteractiveConfigContext';
 const ODD_OPACITY = 1;
 
 const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+  /*
   [`& .${gridClasses.row}.even`]: {
     backgroundColor: theme.palette.mode === 'dark' ? '#333' : '#696a6b',
     '&:hover, &.Mui-hovered': {
@@ -29,54 +30,21 @@ const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
         },
       },
     },
-  },
+  },*/
 }));
 
-export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; csvData: string }): ReactNode => {
+export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; csvData: string[] }): ReactNode => {
+  console.log('Raw Data', csvData);
   const [open, setOpen] = useState(false);
   const [userMessage, setUserMessage] = useState('Surprise me!');
   const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [filteredColumns, setFilteredColumns] = useState([]);
   const [columns, setColumns] = useState([]);
-  const parseCSV = (csvData): void => {
-    let headers = [];
-    const lines = csvData.split('\n');
-    if (lines.length === 2) {
-      return csvData;
-    }
-    const rawHeaders = lines[1].split(',');
-    headers = rawHeaders.map((header) => header.trim());
-    const newRows = [];
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[String(i)].split('","');
-      if (values.length === headers.length) {
-        const row = {} as {
-          id: number;
-        };
-        for (let j = 0; j < headers.length; j++) {
-          values[String(j)] = values[String(j)].trim();
-          if (values[String(j)].startsWith('"') && values[String(j)].endsWith('"')) {
-            values[String(j)] = values[String(j)].slice(1, -1);
-          }
-          if (values[String(j)].startsWith('"')) {
-            values[String(j)] = values[String(j)].slice(1);
-          }
-          if (values[String(j)].endsWith('"')) {
-            values[String(j)] = values[String(j)].slice(0, -1);
-          }
-          row[headers[String(j)]] = values[String(j)];
-        }
-        if (!row.id) {
-          row.id = i;
-        }
-        newRows.push(row);
-      }
-      if (i === 1 && newRows.length > 0) {
-        newRows.shift();
-      }
-    }
-    headers = headers
-      .filter((header) => header !== 'id')
-      .map((header) => ({
+  useEffect(() => {
+    const rawData = csvData.map((row) => row.split(',').map((cell) => cell.trim().replaceAll('"', '')));
+    setColumns(
+      (rawData[0][0].toLowerCase().includes('id') ? rawData[0].slice(1) : rawData[0]).map((header) => ({
         field: header,
         width: Math.max(160, header.length * 10),
         flex: 1,
@@ -89,35 +57,60 @@ export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; 
             textOverflow: 'ellipsis',
           },
         },
-      }));
-    headers = headers.filter((header) => {
-      for (const row of newRows) {
-        if (row[header.field]) {
-          return true;
-        }
-      }
-      return false;
-    });
-    setColumns(headers);
-    setRows(newRows);
-  };
-  useEffect(() => {
-    parseCSV(csvData);
+      })),
+    );
+    setRows(
+      rawData.slice(1).map((row, index) =>
+        rawData[0][0].toLowerCase().includes('id')
+          ? {
+              id: row[0],
+              ...Object.fromEntries(row.slice(1).map((cell, index) => [rawData[0][index + 1], cell])),
+            }
+          : {
+              id: index,
+              ...Object.fromEntries(row.map((cell, index) => [rawData[0][index], cell])),
+            },
+      ),
+    );
   }, [csvData]);
 
+  useEffect(() => {
+    setFilteredRows(rows);
+  }, rows);
+  useEffect(() => {
+    setFilteredColumns(columns);
+  }, columns);
   const getInsights = async (userMessage): Promise<void> => {
-    const lines = csvData.split('\n');
-    lines.shift();
-    lines.pop();
+    const stringifiedColumns = ['id', ...filteredColumns.map((header) => header.field)].join(',');
+    const stringifiedRows = filteredRows.map((row) => columns.map((header) => row[header.field]).join(','));
+    console.log('Data Analysis', [stringifiedColumns, ...stringifiedRows]);
     console.log(
       await state.agixt.runChain('Data Analysis', userMessage, state.agent, false, 1, {
         conversation_name: state.overrides.conversationName,
-        text: lines.join('\n'),
+        text: [stringifiedColumns, ...stringifiedRows].join('\n'),
       }),
     );
   };
+  const gridChange = (state) => {
+    console.log(state);
+    setFilteredRows(
+      rows.filter((row) =>
+        Object.keys(state.filter.filteredRowsLookup)
+          .filter((key) => state.filter.filteredRowsLookup[key])
+          .includes(row.id),
+      ),
+    );
+    setFilteredColumns(
+      columns.filter(
+        (column) =>
+          !Object.keys(state.columns.columnVisibilityModel)
+            .filter((key) => !state.columns.columnVisibilityModel[key])
+            .includes(column.field),
+      ),
+    );
+  };
   return rows.length > 1 ? (
-    <>
+    <Box display='flex' flexDirection='column' gap='1rem'>
       <StripedDataGrid
         density='compact'
         rows={rows}
@@ -131,12 +124,11 @@ export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; 
           },
         }}
         getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd')}
+        onStateChange={gridChange}
       />
-      <br />
       <Box
         sx={{
           display: 'flex',
-          justifyContent: 'flex-end',
         }}
       >
         <Button
@@ -147,7 +139,7 @@ export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; 
           }}
         >
           <TipsAndUpdatesOutlinedIcon />
-          &nbsp;Get Insights
+          Get Insights
         </Button>
       </Box>
       <Dialog
@@ -196,7 +188,7 @@ export const DataGridFromCSV = ({ state, csvData }: { state: InteractiveConfig; 
           </Button>
         </DialogActions>
       </Dialog>
-    </>
+    </Box>
   ) : (
     csvData
   );
