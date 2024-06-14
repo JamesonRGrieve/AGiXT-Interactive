2871cb0a0c57eafd3f3c8975dc728cfefb18f674
James Grieve <jamesonrgrieve@gmail.com>
2024-06-14 06:51:40 -0600

    Add dynamic oauth2;

--- middleware.tsx
+++ middleware.tsx
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { useNextAPIBypass, useJWTQueryParam, useGoogleOAuth2, useAuth } from 'jrgcomponents/Middleware/Hooks';
+import { useNextAPIBypass, useJWTQueryParam, useOAuth2, useAuth } from 'jrgcomponents/Middleware/Hooks';
 
 export default async function Middleware(req: NextRequest): Promise<NextResponse> {
-  const hooks = [useNextAPIBypass, useGoogleOAuth2, useJWTQueryParam, useAuth];
+  const hooks = [useNextAPIBypass, useOAuth2, useJWTQueryParam, useAuth];
   for (const hook of hooks) {
     const hookResult = await hook(req);
     if (hookResult.activated) {
