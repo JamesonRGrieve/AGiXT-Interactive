1d6071fd577a10420fae9aa3133c58a98dd187e5
James Grieve <jamesonrgrieve@gmail.com>
2024-06-20 09:28:21 -0600

    Fix logout button?; Add markdown rendering to activities;

--- components/Chat/Message/Markdown/List.tsx
+++ components/Chat/Message/Markdown/List.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
 export default function renderList({ children, ordered = true }): ReactNode {
-  return ordered ? <ol style={{ paddingLeft: '2em' }}>{children}</ol> : <ul>{children}</ul>;
+  return ordered ? <ol style={{ marginLeft: '1rem' }}>{children}</ol> : <ul style={{ marginLeft: '1rem' }}>{children}</ul>;
 }
--- components/Chat/Message/Message.tsx
+++ components/Chat/Message/Message.tsx
@@ -19,36 +19,8 @@ import { ContentCopy as ContentCopyIcon, Download as DownloadIcon, ThumbUp, Thum
 import clipboardCopy from 'clipboard-copy';
 import { InteractiveConfigContext } from '../../../types/InteractiveConfigContext';
 import MarkdownBlock from './MarkdownBlock';
+import formatDate from './formatDate';
 
-function formatDate(timestamp: string, short: boolean = true): string {
-  // Create a date object from the timestamp
-  const date = new Date(timestamp);
-
-  // Convert the date to the server timezone
-  const serverDate = new Date(
-    date.toLocaleString('en-US', {
-      timeZone: process.env.NEXT_PUBLIC_TZ ? process.env.NEXT_PUBLIC_TZ.replace('TZ-', '') : '',
-    }),
-  );
-
-  // Calculate the time difference between the server date and the original date
-  const timeDifference = date.getTime() - serverDate.getTime();
-
-  // Create a new date object that represents the local time
-  const localDate = new Date(date.getTime() + timeDifference);
-
-  // Format the local date
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: short ? 'short' : 'long',
-    day: '2-digit',
-    hour: short ? 'numeric' : '2-digit',
-    minute: '2-digit',
-    second: short ? undefined : '2-digit',
-    hour12: true,
-  };
-  return localDate.toLocaleString('en-US', options);
-}
 export type MessageProps = {
   chatItem: { role: string; message: string; timestamp: string; rlhf?: { positive: boolean; feedback: string } };
   lastUserMessage: string;
