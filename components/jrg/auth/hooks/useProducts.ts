720f944b5eb47d655703c73a5693b00459ab55e6
James Grieve <jamesonrgrieve@gmail.com>
2025-01-20 09:19:23 -0700

    Bypass products with no stripe key;

--- components/jrg/auth/hooks/useProducts.ts
+++ components/jrg/auth/hooks/useProducts.ts
@@ -7,15 +7,17 @@ export default function useProducts() {
   return useSWR(
     '/products',
     async () =>
-      (
-        await axios.get(`${process.env.NEXT_PUBLIC_AGIXT_SERVER}/v1/products`, {
-          headers: {
-            Authorization: getCookie('jwt'),
-          },
-        })
-      ).data
-        .map((x: any) => ({ ...x }))
-        .sort((a: any, b: any) => (a.last_name > b.last_name ? 1 : -1)),
+      process.env.STRIPE_PUBLISHABLE_KEY
+        ? (
+            await axios.get(`${process.env.NEXT_PUBLIC_AGIXT_SERVER}/v1/products`, {
+              headers: {
+                Authorization: getCookie('jwt'),
+              },
+            })
+          ).data
+            .map((x: any) => ({ ...x }))
+            .sort((a: any, b: any) => (a.last_name > b.last_name ? 1 : -1))
+        : [],
     {
       fallbackData: [],
     },
