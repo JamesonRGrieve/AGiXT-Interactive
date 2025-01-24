3ac42adcdad78473b3d5c5b9b5b6b66903313537
James Grieve <jamesonrgrieve@gmail.com>
2025-01-24 08:51:02 -0700

    Uncomment youtube;

--- components/interactive/Chat/Message/Markdown/Link.tsx
+++ components/interactive/Chat/Message/Markdown/Link.tsx
@@ -5,17 +5,17 @@ import { cn } from '@/lib/utils';
 // import Plyr from 'plyr-react';
 // import 'plyr-react/plyr.css';
 
-// const handleAnchorClick = (e: React.MouseEvent<HTMLAnchorElement>): void => {
-//   const href = e.currentTarget.getAttribute('href');
-//   if (href?.startsWith('#')) {
-//     e.preventDefault();
-//     const id = href.slice(1);
-//     const element = document.getElementById(id);
-//     if (element) {
-//       element.scrollIntoView({ behavior: 'smooth' });
-//     }
-//   }
-// };
+const handleAnchorClick = (e: React.MouseEvent<HTMLAnchorElement>): void => {
+  const href = e.currentTarget.getAttribute('href');
+  if (href?.startsWith('#')) {
+    e.preventDefault();
+    const id = href.slice(1);
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+};
 
 const getYoutubeId = (url: string): string | null => {
   const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
@@ -31,20 +31,21 @@ const MarkdownLink: React.FC<MarkdownLinkProps> = ({ children, href, className, 
   const youtubeId = href ? getYoutubeId(href) : null;
   const isVideo = href?.match(/\.(mp4|webm|ogg)$/i);
 
-  // if (youtubeId) {
-  //   return (
-  //     <div className='w-96'>
-  //       <div className='relative w-full aspect-video'>
-  //         <iframe
-  //           className='absolute top-0 left-0 w-full h-full'
-  //           src={`https://www.youtube.com/embed/${youtubeId}`}
-  //           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-  //           allowFullScreen
-  //         />
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (youtubeId) {
+    return (
+      <div className='w-96'>
+        <div className='relative w-full aspect-video'>
+          <iframe
+            title={youtubeId}
+            className='absolute top-0 left-0 w-full h-full'
+            src={`https://www.youtube.com/embed/${youtubeId}`}
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          />
+        </div>
+      </div>
+    );
+  }
 
   // if (isVideo) {
   //   return (
@@ -79,8 +80,8 @@ const MarkdownLink: React.FC<MarkdownLinkProps> = ({ children, href, className, 
   );
 };
 
-// function getYoutubeId(url: string): string | null {
-//   const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-//   const match = url.match(regExp);
-//   return match && match[7].length === 11 ? match[7] : null;
-// }
+function getYoutubeId(url: string): string | null {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length === 11 ? match[7] : null;
+}
