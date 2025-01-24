5bd1893259a0dfb3b2639a58cee58e8b127b8689
waiscodes <b.wais@outlook.com>
2025-01-24 09:20:13 -0700

    add notifications page

---
+++ app/notifications/page.tsx
@@ -0,0 +1,10 @@
+import { SidebarHeader, SidebarHeaderTitle } from '@/components/jrg/appwrapper/SidebarHeader';
+import { SidebarMain } from '@/components/jrg/appwrapper/SidebarMain';
+
+export default function NotificationsPage() {
+  return (
+    <div>
+      <h1>Notifications</h1>
+    </div>
+  );
+}
