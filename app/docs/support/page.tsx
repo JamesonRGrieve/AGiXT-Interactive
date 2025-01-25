d2696b15dafe06d926a0127657d0f9b7b4697d5f
James Grieve <jamesonrgrieve@gmail.com>
2025-01-25 16:22:00 -0700

    Update alerts;

--- app/docs/support/page.tsx
+++ app/docs/support/page.tsx
@@ -31,17 +31,22 @@ export default function SupportPage() {
       <SidebarHeader>
         <SidebarHeaderTitle>Support Request</SidebarHeaderTitle>
       </SidebarHeader>
-      <Alert>
-        <AlertTitle>Early Access Software</AlertTitle>
-        <AlertDescription>
-          This is an early-access deployment of open-source software. You may encounter problems or &quot;bugs&quot;. If you
-          do, please make note of your most recent actions and{' '}
-          <Link href='https://github.com/JamesonRGrieve/AGiXT-Interactive/issues/new?template=bug_report_prod.yml'>
-            let us know by making a report here
-          </Link>
-          . Your understanding as we build towards the future is much appreciated.
-        </AlertDescription>
-      </Alert>
+      <div className='mb-4'>
+        <Alert>
+          <AlertTitle>Early Access Software</AlertTitle>
+          <AlertDescription>
+            This is an early-access deployment of open-source software. You may encounter problems or &quot;bugs&quot;. If
+            you do, please make note of your most recent actions and{' '}
+            <Link
+              className='text-info hover:underline'
+              href='https://github.com/JamesonRGrieve/AGiXT-Interactive/issues/new?template=bug_report_prod.yml'
+            >
+              let us know by making a report here
+            </Link>
+            . Your understanding as we build towards the future is much appreciated.
+          </AlertDescription>
+        </Alert>
+      </div>
       <SidebarMain>
         <MarkdownBlock content={content} />
       </SidebarMain>
--- components/jrg/auth/Subscribe.tsx
+++ components/jrg/auth/Subscribe.tsx
@@ -24,17 +24,22 @@ export default function Subscribe({ searchParams }: { searchParams: any }): JSX.
 
   return (
     <>
-      <Alert>
-        <AlertTitle>Early Access Software</AlertTitle>
-        <AlertDescription>
-          This is an early-access deployment of open-source software. You may encounter problems or &quot;bugs&quot;. If you
-          do, please make note of your most recent actions and{' '}
-          <Link href='https://github.com/JamesonRGrieve/AGiXT-Interactive/issues/new?template=bug_report_prod.yml'>
-            let us know by making a report here
-          </Link>
-          . Your understanding as we build towards the future is much appreciated.
-        </AlertDescription>
-      </Alert>
+      <div className='mb-4'>
+        <Alert>
+          <AlertTitle>Early Access Software</AlertTitle>
+          <AlertDescription>
+            This is an early-access deployment of open-source software. You may encounter problems or &quot;bugs&quot;. If
+            you do, please make note of your most recent actions and{' '}
+            <Link
+              className='text-info hover:underline'
+              href='https://github.com/JamesonRGrieve/AGiXT-Interactive/issues/new?template=bug_report_prod.yml'
+            >
+              let us know by making a report here
+            </Link>
+            . Your understanding as we build towards the future is much appreciated.
+          </AlertDescription>
+        </Alert>
+      </div>
       {authConfig.subscribe.heading && <h2 className='text-3xl'>{authConfig.subscribe.heading}</h2>}
       {process.env.NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID ? (
         <Suspense fallback={<p>Loading pricing...</p>}>
