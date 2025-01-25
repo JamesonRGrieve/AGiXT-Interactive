6bde8c788a68bb9a2ebd138eab3a32bee754c364
James Grieve <jamesonrgrieve@gmail.com>
2025-01-25 16:22:52 -0700

    Add padding to docs pages;

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,5 +1,9 @@
 import { SidebarInset } from '@/components/ui/sidebar';
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return <SidebarInset>{children}</SidebarInset>;
+  return (
+    <SidebarInset>
+      <div className='p-4'>{children}</div>
+    </SidebarInset>
+  );
 }
